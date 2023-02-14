c7d4934fcac101705ccf6fd380acac53400232de
IvanRomero03 <i.wells.ar@gmail.com>
2023-02-13 22:14:53 -0600

    front miadin

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,9 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      "animate-slow": "pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite",
+    },
   },
   plugins: [],
 };
