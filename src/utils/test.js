1c8fdc0b9edba90ea39612e964ec4b5a2e77b7fc
Oscar-gg <oscar.arreola.jr@gmail.com>
2023-02-13 22:45:29 -0600

    Files

---
+++ src/utils/test.js
@@ -0,0 +1,10 @@
+const date = new Date(Date.now())
+
+const dateString = "" + date.getFullYear();
+
+if (date.getMonth() > 6)
+    dateString += '13';
+else{
+    dateString += '11';   
+}
+console.log(dateString);
