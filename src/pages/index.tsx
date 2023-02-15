1b2b182ad82a451f18a1dbce0b22e7fee9a05024
IvanRomero03 <i.wells.ar@gmail.com>
2023-02-15 17:08:12 -0600

    changes to submitions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,26 +11,30 @@ const Home: NextPage = () => {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   const handleSubmit = async () => {
-    const dataResponse = await axios.get(
-      "https://alsvdes01.itesm.mx:8282/tmty/alumnos/" +
-        matricula +
-        "/horario/tec21?cveTerm=" +
-        "202311",
-      {
-        headers: {
-          Authorization: "Basic c1NJU1RFTUEtUE9SVEFMOmMwNTExQnA0Nlo=",
-          "Content-Type": "application/vnd.api+json",
-          Accept: "application/vnd.api+json",
-        },
+    try {
+      const dataResponse = await axios.get(
+        "https://alsvdes01.itesm.mx:8282/tmty/alumnos/" +
+          matricula +
+          "/horario/tec21?cveTerm=" +
+          "202311",
+        {
+          headers: {
+            Authorization: "Basic c1NJU1RFTUEtUE9SVEFMOmMwNTExQnA0Nlo=",
+            "Content-Type": "application/vnd.api+json",
+            Accept: "application/vnd.api+json",
+          },
+        }
+      );
+      if (dataResponse?.status !== 200) {
+        alert("No se encontró el horario");
+        return;
       }
-    );
-    if (dataResponse.status !== 200) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      const icsText = String(icsTransformer(dataResponse?.data?.data));
+      window.open("data:text/calendar;charset=utf8," + escape(icsText));
+    } catch (error) {
       alert("No se encontró el horario");
-      return;
     }
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-    const icsText = String(icsTransformer(dataResponse?.data?.data));
-    window.open("data:text/calendar;charset=utf8," + escape(icsText));
   };
   return (
     <>
@@ -39,8 +43,8 @@ const Home: NextPage = () => {
         <meta name="description" content="By" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="h-screen">
-        <main className="flex min-h-screen  flex-col items-center justify-center bg-gradient-to-r from-gray-800 via-purple-500 to-blue-900 duration-700">
+      <div className="absolute inset-0 h-full">
+        <main className="flex min-h-full min-w-max  flex-col items-center justify-center bg-gradient-to-r from-gray-800 via-purple-500 to-blue-900 duration-700">
           <div className="flex animate-none flex-col items-center justify-center rounded-lg bg-gray-300 text-opacity-100">
             <h1 className="m-12 animate-none text-4xl font-bold text-gray-600">
               Convertidor Horario
